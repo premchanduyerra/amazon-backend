d7430b8ba41afd25b6d9bffd47cba76bdd940905
Prem Chandu <47866350+premchanduyerra@users.noreply.github.com>
2020-09-16 00:26:31 +0530

    amazon backend

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 
 app.get('/', (request, response) => {
-    response.status(200).send("hello")
+    response.send("hello")
 })
 app.post("/payments/create", async (request, response) => {
     const total = request.query.total;
